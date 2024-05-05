1dae82f73882586bed8734cd507db31301064408
Nikita <dubblerner.2@gmail.com>
2024-05-05 19:55:11 +0300

    finished delivery page

--- js/main.js
+++ js/main.js
@@ -13,3 +13,22 @@ function mobMenuToggle() {
     }
    
 }
+
+
+const contactUsModal = document.getElementById('contactUsModal');
+const contactShowModal = document.getElementById('contactUsBtn');
+
+contactShowModal.addEventListener('click', showModal);
+
+function showModal() {
+    contactUsModal.classList.remove('hidden')
+}
+
+
+const closeContactUsModal = document.querySelector('.contactUs-modal__close');
+
+closeContactUsModal.addEventListener('click', closeModal)
+
+function closeModal() {
+    contactUsModal.classList.add('hidden');
+}
